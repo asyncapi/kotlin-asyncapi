6e8db657617c4533f5957e892178b368e44be342
lorenzsimon <lorenz.simon.mail@gmail.com>
2023-10-01 13:04:34 +0200

    fix: Test

--- kotlin-asyncapi-examples/kotlin-asyncapi-spring-boot-example/src/test/kotlin/org/openfolder/kotlinasyncapi/example/spring/ExampleApplicationTests.kt
+++ kotlin-asyncapi-examples/kotlin-asyncapi-spring-boot-example/src/test/kotlin/org/openfolder/kotlinasyncapi/example/spring/ExampleApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.example.demo
+package org.openfolder.kotlinasyncapi.example.spring
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
@@ -9,5 +9,4 @@ class ExampleApplicationTests {
 	@Test
 	fun contextLoads() {
 	}
-
 }
